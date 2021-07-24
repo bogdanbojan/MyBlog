4c30aea876a2382cbdd415104c2cda909bdb5dbd
bogdanbojan <71257990+bogdanbojan@users.noreply.github.com>
2021-07-24 20:21:15 +0300

    Added dependencies. Added a POST method as well.

--- src/Handler/PostNew.hs
+++ src/Handler/PostNew.hs
@@ -1,6 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Handler.PostNew where
 
+import Yesod.Form.Bootstrap3
+import Text.Markdown (Markdown)
+import Yesod.Text.Markdown
 import Import
 
+
+data BlogPost = BlogPost
+        { title :: Text
+        , article :: Markdown
+        }
+
+blogPostForm :: AForm Handler BlogPost
+blogPostForm = BlogPost 
+            <$> areq textField (bfs ("Title" :: Text)) Nothing
+            <*> areq markdownField (bfs ("Article" :: Text)) Nothing
+
 getPostNewR :: Handler Html
-getPostNewR = error "Not yet implemented: getPostNewR"
+getPostNewR = do
+    (widget, enctype) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm blogPostForm
+    defaultLayout $ do
+        $(widgetFile "posts/new")
