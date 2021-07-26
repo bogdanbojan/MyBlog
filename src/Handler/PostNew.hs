1f386ff67851afa7279f97d40c60360d2f74a7f2
bogdanbojan <71257990+bogdanbojan@users.noreply.github.com>
2021-07-26 17:32:49 +0300

    end of tutorial

--- src/Handler/PostNew.hs
+++ src/Handler/PostNew.hs
@@ -3,15 +3,16 @@
 module Handler.PostNew where
 
 import Yesod.Form.Bootstrap3
-import Text.Markdown (Markdown)
 import Yesod.Text.Markdown
 import Import
 
 
-data BlogPost = BlogPost
-        { title :: Text
-        , article :: Markdown
-        }
+-- data BlogPost = BlogPost
+--         { title :: Text
+--         , article :: Markdown
+--         }
+-- Same record datatype has been generated from models.
+-- Therefore, we don't really need this one.
 
 blogPostForm :: AForm Handler BlogPost
 blogPostForm = BlogPost 
@@ -23,3 +24,12 @@ getPostNewR = do
     (widget, enctype) <- generateFormPost $ renderBootstrap3 BootstrapBasicForm blogPostForm
     defaultLayout $ do
         $(widgetFile "posts/new")
+
+postPostNewR :: Handler Html
+postPostNewR = do
+        ((res, widget), enctype) <- runFormPost $ renderBootstrap3 BootstrapBasicForm blogPostForm
+        case res of 
+          FormSuccess blogPost -> do 
+                blogPostId <- runDB $ insert blogPost 
+                redirect $ PostDetailsR blogPostId
+          _ -> defaultLayout $(widgetFile "posts/new")
